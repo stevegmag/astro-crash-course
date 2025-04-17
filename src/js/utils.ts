86a75ab49f2f4f9428bfd2ee224ae58f92bafe7d
Steven Gallagher <stevegmag@gmail.com>
2025-04-17 18:24:57 -0400

    updated ts and pathing to utils

---
+++ src/js/utils.ts
@@ -0,0 +1,22 @@
+export function copyright(): string {
+  return '© Copyright ' + (new Date().getFullYear());
+}
+
+export function slugify(text: string): string {
+  return text
+    .toString()
+    .toLowerCase()
+    .replace(/\s+/g, '-')
+    .replace(/[^\w-]+/g, '')
+    .replace(/--+/g, '-')
+    .replace(/^-+/, '')
+    .replace(/-+$/, '');
+}
+
+export function formatDate(date: Date): string {
+  return new Intl.DateTimeFormat('en-US', {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric'
+  }).format(date);
+}
